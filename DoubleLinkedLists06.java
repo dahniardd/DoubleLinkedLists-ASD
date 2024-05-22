980b2b92bbadfb2ae5391a0aa77a8352180fec60
dahniardd <143977411+dahniardd@users.noreply.github.com>
2024-05-22 13:38:20 +0700

    Add files via upload

--- DoubleLinkedLists06.java
+++ DoubleLinkedLists06.java
@@ -1,5 +1,7 @@
 package doublelinkedlists;
 
+import org.w3c.dom.Node;
+
 public class DoubleLinkedLists06 {
     Node06 head;
     int size;
@@ -79,4 +81,82 @@ public class DoubleLinkedLists06 {
             System.out.println("Linked Lists Kosong");
         }
     }
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (size == 1) {
+            removeLast();
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List masih kosong, tidak dapat dihapus!");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node06 current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+    public void remove (int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node06 current = head;
+            int i = 0;
+            while (i < index) {
+            current = current.next;
+            i++;
+        }
+        if (current.next == null) {
+            current.prev.next = null;
+        } else if (current.prev == null) {
+            current = current.next; 
+            current.prev = null;
+            head = current;
+        } else {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+        }
+        size--;
+        }
+    }
+    public int getFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List kosong");
+        }
+        return head.data;
+    }
+    public int getLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked List kosong");
+        }
+            Node06 tmp = head;
+            while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+    public int get(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas.");
+        }
+        Node06 tmp = head; 
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
 }
+
