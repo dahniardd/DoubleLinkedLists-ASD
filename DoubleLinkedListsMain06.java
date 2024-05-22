8a2fd1d7ca382a1f0c655f420385275deca56f83
dahniardd <143977411+dahniardd@users.noreply.github.com>
2024-05-22 12:23:28 +0700

    Add files via upload

---
+++ DoubleLinkedListsMain06.java
@@ -0,0 +1,23 @@
+package doublelinkedlists;
+
+public class DoubleLinkedListsMain06 {
+    public static void main(String[]args) throws Exception {
+        DoubleLinkedLists06 dll = new DoubleLinkedLists06();
+        dll.print();
+        System.out.println("Size: "+dll.size());
+        System.out.println("=========================================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size: "+dll.size());
+        System.out.println("=========================================");
+        dll.add(40,1);
+        dll.print();
+        System.out.println("Size: "+dll.size());
+        System.out.println("=========================================");
+        dll.clear();
+        dll.print();
+        System.out.println("Size: "+dll.size());
+    }
+}
